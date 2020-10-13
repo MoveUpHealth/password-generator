1bfa91e56f28ce5ee98f37f86aef108e9e0fba0b
Coach Jess <69859383+MoveUpHealth@users.noreply.github.com>
2020-10-13 15:02:58 -0700

    created arrays for password generator

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 // Assignment Code
+//Create arrays for password content: Uppercase, lowercase, numbers, special characters
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var special = [' ', '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', ',', '-', '.', '/', ':', ';', '<', '?', '[', ']', '\'', '_', '|', '{', '}', '"', "'"]
+var lowercase = []
+
+for (var i= 0; i < uppercase.length; i++){
+  lowercase.push(uppercase[i].toLowerCase());
+}
+
+var combination = []
+
+for (var x= 0; x < uppercase.length; x++){
+  combination.push(uppercase[x]);
+}
+for (var y= 0; y < number.length; y++){
+  combination.push(number[y]);
+}
+for (var z= 0; z < special.length; z++){
+  combination.push(special[z]);
+}
+for (var a= 0; a < lowercase.length; a++){
+  combination.push(lowercase[a]);
+}
+
+console.log(combination)
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
